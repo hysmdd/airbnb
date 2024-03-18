588d9cfb9fa953e1f71b17bf3c941ce5a335a968
qinhao <coderqin@foxmail.com>
2024-03-18 15:36:49 +0800

    Router的搭建与配置

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/App";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "@/App";
 import "normalize.css";
 import "@/assets/css/index.less";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="loading">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Suspense>
   </React.StrictMode>
 );
 
