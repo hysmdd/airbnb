a4b3145fdaaee32d42de593f79f7f7a99789ec99
qinhao <coderqin@foxmail.com>
2024-03-25 16:20:16 +0800

    页码组件展示与展示数据算法

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,8 +1,29 @@
 import React, { memo } from "react";
+import Pagination from "@mui/material/Pagination";
 import { PaginationWrapper } from "./style";
+import { useSelector } from "react-redux";
 
 const EntirePagination = memo(() => {
-  return <PaginationWrapper>EntirePagination</PaginationWrapper>;
+  const { totalCount, currentPage, roomList } = useSelector((state) => ({
+    totalCount: state.entire.totalCount,
+    currentPage: state.entire.currentPage,
+    roomList: state.entire.roomList,
+  }));
+  const totalPage = Math.ceil(totalCount / 20);
+  const startCount = currentPage * 20 + 1;
+  const endCount = (currentPage + 1) * 20;
+  return (
+    <PaginationWrapper>
+      {!!roomList.length && (
+        <div className="info">
+          <Pagination count={totalPage} />
+          <div className="desc">
+            第 {startCount} - {endCount} 个房源，共超过 {totalCount} 个
+          </div>
+        </div>
+      )}
+    </PaginationWrapper>
+  );
 });
 
 export default EntirePagination;
--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -1,3 +1,33 @@
 import styled from "styled-components";
 
-export const PaginationWrapper = styled.div``
+export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .MuiPaginationItem-icon {
+      font-size: 24px;
+    }
+
+    .MuiPaginationItem-page {
+      margin: 0 9px;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    .MuiPaginationItem-page.Mui-selected {
+      background: #222;
+      color: #fff;
+    }
+    .desc {
+      margin-top: 16px;
+      color: #222;
+    }
+  }
+`;
