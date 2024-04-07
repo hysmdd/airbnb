379b21d51271895cf4d25b15158e8993831f2964
qinhao <coderqin@foxmail.com>
2024-04-07 10:09:10 +0800

    图片浏览器中间的图片展示

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { BrowserWrapper } from "./style";
 import IconClose from "@/assets/svg/icon_close";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props;
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   // 当图片浏览器展示出来的时候，应该让滚动条功能消失
   useEffect(() => {
@@ -21,6 +24,18 @@ const PictureBrowser = memo((props) => {
     }
   }
 
+  function controlClickHandle(isNext = true) {
+    let newIndex = isNext ? currentIndex + 1 : currentIndex - 1;
+    if (newIndex < 0) {
+      newIndex = pictureUrls.length - 1;
+    }
+    if (newIndex > pictureUrls.length - 1) {
+      newIndex = 0;
+    }
+    console.log("切换图片: ", newIndex);
+    setCurrentIndex(newIndex);
+  }
+
   return (
     <BrowserWrapper>
       <div className="top">
@@ -28,8 +43,20 @@ const PictureBrowser = memo((props) => {
           <IconClose />
         </div>
       </div>
-      <div className="list"></div>
-      <div className="indicator"></div>
+      <div className="slider">
+        <div className="control">
+          <div className="btn left" onClick={(e) => controlClickHandle(false)}>
+            <IconArrowLeft width="77" height="77" />
+          </div>
+          <div className="btn right" onClick={(e) => controlClickHandle(true)}>
+            <IconArrowRight width="77" height="77" />
+          </div>
+        </div>
+        <div className="picture">
+          <img src={pictureUrls[currentIndex]} alt="" />
+        </div>
+      </div>
+      <div className="preview"></div>
     </BrowserWrapper>
   );
 });
--- src/base-ui/picture-browser/style.js
+++ src/base-ui/picture-browser/style.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const BrowserWrapper = styled.div`
   position: fixed;
+  display: flex;
+  flex-direction: column;
   z-index: 999;
   top: 0;
   right: 0;
@@ -19,5 +21,58 @@ export const BrowserWrapper = styled.div`
       right: 25px;
       cursor: pointer;
     }
+  }
+
+  .slider {
+    flex: 1;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+
+    .control {
+      position: absolute;
+      z-index: 1;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      display: flex;
+      justify-content: space-between;
+      color: #fff;
+
+      .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 83px;
+        height: 100%;
+        cursor: pointer;
+      }
+    }
+
+    .picture {
+      position: relative;
+      height: 100%;
+      overflow: hidden;
+      width: 100%;
+      max-width: 105vh;
+
+      img {
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        margin: 0 auto;
+        height: 100%;
+        user-select: none;
+      }
+    }
+  }
+
+  .preview {
+    height: 100px;
+    margin-top: 10px;
   }
 `;
